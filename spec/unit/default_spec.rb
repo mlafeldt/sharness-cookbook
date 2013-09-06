cec9f1d38adab9cd6723be036825718c1db0f98d
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-09-07 00:12:07 +0200

    Add unit tests

--- spec/unit/default_spec.rb
+++ spec/unit/default_spec.rb
@@ -12,6 +12,23 @@ describe 'The recipe sharness::default' do
     chef_run.converge 'sharness::default'
     chef_run
   end
+  let (:tmp_dir) { Chef::Config[:file_cache_path] }
 
-  it 'installs Sharness'
+  it 'creates a temporary directory to store build products' do
+    chef_run.should create_directory tmp_dir
+  end
+
+  it 'downloads the sharness source tarball' do
+    version = chef_run.node['sharness']['version']
+    tarball = File.join(tmp_dir, "sharness-#{version}.tar.gz")
+    chef_run.should create_remote_file tarball
+  end
+
+  it 'installs make' do
+    chef_run.should install_package 'make'
+  end
+
+  it 'installs sharness' do
+    chef_run.should execute_bash_script 'install sharness'
+  end
 end
