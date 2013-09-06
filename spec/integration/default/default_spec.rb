ba2fdb923082adce926d062279232bc1cda27c0e
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-09-07 00:32:52 +0200

    Add integration tests

--- spec/integration/default/default_spec.rb
+++ spec/integration/default/default_spec.rb
@@ -1,5 +1,20 @@
 require 'chef_bones/integration_spec_helper'
 
-describe 'The sharness::default recipe' do
-  it "installs Sharness"
+describe 'The recipe sharness::default' do
+  let (:prefix) { '/usr/local' }
+
+  it 'installs sharness' do
+    expect(file "#{prefix}/share/sharness/sharness.sh").to be_file
+    expect(file "#{prefix}/share/sharness/aggregate-results.sh").to be_file
+  end
+
+  it 'installs documentation' do
+    expect(file "#{prefix}/share/doc/sharness/README.md").to be_file
+    expect(file "#{prefix}/share/doc/sharness/COPYING").to be_file
+  end
+
+  it 'installs working examples' do
+    expect(file "#{prefix}/share/doc/sharness/examples/Makefile").to be_file
+    expect(command "sudo make -C #{prefix}/share/doc/sharness/examples").to return_exit_status 0
+  end
 end
