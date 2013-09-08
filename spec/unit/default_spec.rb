40941580cb82ef4312e5fbb5fa799ad4f168a626
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-09-08 18:14:05 +0200

    Use new expect syntax in unit tests

--- spec/unit/default_spec.rb
+++ spec/unit/default_spec.rb
@@ -15,25 +15,25 @@ describe 'The recipe sharness::default' do
   let (:tmp_dir) { Chef::Config[:file_cache_path] }
 
   it 'creates a temporary directory to store build products' do
-    chef_run.should create_directory tmp_dir
+    expect(chef_run).to create_directory tmp_dir
   end
 
   it 'downloads the sharness source tarball' do
     version = chef_run.node['sharness']['version']
     tarball = File.join(tmp_dir, "sharness-#{version}.tar.gz")
-    chef_run.should create_remote_file tarball
+    expect(chef_run).to create_remote_file tarball
   end
 
   it 'installs make' do
-    chef_run.should install_package 'make'
+    expect(chef_run).to install_package 'make'
   end
 
   it 'installs sharness' do
-    chef_run.should execute_bash_script 'install sharness'
+    expect(chef_run).to execute_bash_script 'install sharness'
   end
 
-  it 'sets up environment variables' do
+  it 'sets up environment variable SHARNESS_PATH' do
     content = "export SHARNESS_PATH=\"#{chef_run.node['sharness']['path']}\""
-    chef_run.should create_file_with_content '/etc/profile.d/sharness.sh', content
+    expect(chef_run).to create_file_with_content '/etc/profile.d/sharness.sh', content
   end
 end
